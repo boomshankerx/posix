b24d48ea00a4edc4e63906fd3a1aae7eb1d6025d
Lorne Guse <lorne@techgsolution.com>
2018-05-31 23:27:45 -0600

    Remastered config files

--- common/scripts/getposix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-scp lorne@server.techg:/mnt/data/repos/posix ~
-scp lorne@server.techg:/mnt/data/repos/vim ~
-
---
+++ home/.oh-my-zsh/custom/techg.zsh
@@ -0,0 +1,18 @@
+##DEBIAN##
+
+#VIM
+#alias vi='vim'
+alias v='vim'
+alias vi='gvim'
+
+#DEBIAN APT
+alias au='apt update'
+alias auu='apt update && apt upgrade'
+alias ag='sudo apt-get'
+alias agi='sudo apt-get -y install'
+
+#TMUXINATOR
+export EDITOR=vim
+alias tmuxr=tmuxinator
+
+####
