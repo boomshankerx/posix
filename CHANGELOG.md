533515ee61abe9d6ac60da477788324028e6db4c
Lorne Guse <boomshankerx@hotmail.com>
2025-09-26 13:43:59 -0600

    fix: Added btop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,13 @@
 ### 🐛 Bug Fixes
 
 - Neovim: added mini.align
+- Docker: Added gpg dependency install
+- Added alias r='sudo -e'
+- Added docker alias de and load TMUX on login
+- Added px alias for posix
+- Automatically download docker repo with docker install
+- Fixed bug in docker plugin. Added static-ip plugin
+- Added btop
 ## [1.1.0] - 2025-08-30
 
 ### 🚀 Features
--- etc/.oh-my-zsh/custom/techg.zsh
+++ etc/.oh-my-zsh/custom/techg.zsh
@@ -39,7 +39,7 @@ alias scdr='sudo systemctl daemon-reload'
 # DOCKER
 alias de='docker exec -it'
 alias dcupl='docker compose up -d && docker compose logs -f'
-alias dpsl='docker ps --format "table {{.ID}}\t{{.Names}}"'
+alias dpsl='docker ps --format "table {{.ID}}\t{{.Names}}" | tail -n +2 | sort -k2'
 alias dpsp='docker ps --format "table {{.Names}},{{.Ports}}"'
 alias dpss='docker ps | less -S'
 dpspp (){
